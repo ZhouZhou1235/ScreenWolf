34b3d91ca4d3357afb92224138c4ee07c9d71b34
ZhouZhou1235 <1479499289@qq.com>
2025-08-03 16:44:16 +0800

    【添加】创建第一只宠物 - 小蓝狗

--- mod/pet-demo/src/main/java/com/pinkcandy/Pet_Demo.java
+++ mod/pet-demo/src/main/java/com/pinkcandy/Pet_Demo.java
@@ -1,10 +1,9 @@
-package com.pinkcandy.wolfpet;
+package com.pinkcandy;
 
-import com.pinkcandy.Launcher;
 import com.pinkcandy.screenwolf.base.PetBase;
 
 // 桌宠示例
-public class WolfPet extends PetBase {
+public class Pet_Demo extends PetBase {
 
     // 创建自己的成员变量
     // protected boolean isPlaying;
@@ -12,7 +11,7 @@ public class WolfPet extends PetBase {
 
     // 【必须】构造方法 必须重写并调用父类构造
     // 【重要】类名是宠物的唯一标识，应该避免冲突。
-    public WolfPet(Launcher launcher){super(launcher);}
+    public Pet_Demo(Launcher launcher){super(launcher);}
 
     // 重写以实现自定义行为
 
---
+++ mod/skyblue-hound-zhou/src/main/java/com/pinkcandy/Pet_SkyblueHoundZhou.java
@@ -0,0 +1,10 @@
+package com.pinkcandy;
+
+import com.pinkcandy.screenwolf.base.PetBase;
+
+// 小蓝狗周周
+public class Pet_SkyblueHoundZhou extends PetBase {
+    public Pet_SkyblueHoundZhou(Launcher theLauncher){
+        super(theLauncher);
+    }
+}
